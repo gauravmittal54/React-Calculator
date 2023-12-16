042ad4439eb297b131cb485c929be8a9f8c51454
Gaurav Mittal <61792468+gauravmittal54@users.noreply.github.com>
2023-12-16 10:32:58 +0530

    Update DigitButton.js

--- src/Components/DigitButton.js
+++ src/Components/DigitButton.js
@@ -2,6 +2,7 @@ import { ACTIONS } from '../App'
 
 function DigitButton(props) {
     return (
+        //sets digit to payload for their availability to reducer method
         <button onClick={()=>props.dispatch({type:ACTIONS.ADD_DIGIT , payload:{digit:props.digit}})}>{props.digit}</button>
     );
 }
