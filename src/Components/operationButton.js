508b8047e6dc03b0c5233ea8bbd0bab6d0bb7de9
Gaurav Mittal <61792468+gauravmittal54@users.noreply.github.com>
2023-12-16 10:33:12 +0530

    Update operationButton.js

--- src/Components/operationButton.js
+++ src/Components/operationButton.js
@@ -2,6 +2,7 @@ import { ACTIONS } from '../App'
 
 function OperationButton(props) {
     return (
+        //sets digit to payload for their availability to reducer method
         <button onClick={()=>props.dispatch({type:ACTIONS.CHOOSE_OPERATION , payload:{operation:props.operation}})}>{props.operation}</button>
     );
 }
